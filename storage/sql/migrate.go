8b2dd1a4326b5c3fff9ea83cbbc7f93984dd114c
Jeff Haynie <jhaynie@gmail.com>
2017-05-20 11:40:06 -0700

    prefix all sql tables with dex_

--- storage/sql/migrate.go
+++ storage/sql/migrate.go
@@ -71,7 +71,7 @@ type migration struct {
 var migrations = []migration{
 	{
 		stmts: []string{`
-			create table client (
+			create table dex_client (
 				id text not null primary key,
 				secret text not null,
 				redirect_uris bytea not null, -- JSON array of strings
@@ -81,7 +81,7 @@ var migrations = []migration{
 				logo_url text not null
 			);`,
 			`
-			create table auth_request (
+			create table dex_auth_request (
 				id text not null primary key,
 				client_id text not null,
 				response_types bytea not null, -- JSON array of strings
@@ -107,7 +107,7 @@ var migrations = []migration{
 				issuer_url text
 			);`,
 			`
-			create table auth_code (
+			create table dex_auth_code (
 				id text not null primary key,
 				client_id text not null,
 				scopes bytea not null, -- JSON array of strings
@@ -126,7 +126,7 @@ var migrations = []migration{
 				expiry timestamptz not null
 			);`,
 			`
-			create table refresh_token (
+			create table dex_refresh_token (
 				id text not null primary key,
 				client_id text not null,
 				scopes bytea not null, -- JSON array of strings
@@ -142,7 +142,7 @@ var migrations = []migration{
 				connector_data bytea
 			);`,
 			`
-			create table password (
+			create table dex_password (
 				email text not null primary key,
 				hash bytea not null,
 				username text not null,
@@ -150,7 +150,7 @@ var migrations = []migration{
 			);`,
 			`
 			-- keys is a weird table because we only ever expect there to be a single row
-			create table keys (
+			create table dex_keys (
 				id text not null primary key,
 				verification_keys bytea not null, -- JSON array
 				signing_key bytea not null,       -- JSON object
@@ -161,19 +161,19 @@ var migrations = []migration{
 	},
 	{
 		stmts: []string{`
-			alter table refresh_token
+			alter table dex_refresh_token
 				add column token text not null default '';`,
 			`
-			alter table refresh_token
+			alter table dex_refresh_token
 				add column created_at timestamptz not null default '0001-01-01 00:00:00 UTC';`,
 			`
-			alter table refresh_token
+			alter table dex_refresh_token
 				add column last_used timestamptz not null default '0001-01-01 00:00:00 UTC';`,
 		},
 	},
 	{
 		stmts: []string{`
-			create table offline_session (
+			create table dex_offline_session (
 				user_id text not null,
 				conn_id text not null,
 				refresh bytea not null,
@@ -183,7 +183,7 @@ var migrations = []migration{
 	},
 	{
 		stmts: []string{`
-			create table connector (
+			create table dex_connector (
 				id text not null primary key,
 				type text not null,
 				name text not null,
